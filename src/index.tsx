de57efb0e75e10c465e4e2f65e72a20fb9701fa6
Élise Philippe <eriizu@t0.eriizu.fr>
2021-01-13 15:51:53 +0100

    trying location and notification registration

--- src/index.tsx
+++ src/index.tsx
@@ -15,9 +15,16 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
+Notification.requestPermission();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+navigator.geolocation.getCurrentPosition(function(position) {
+  console.log(position);
+  // console.log("Latitude is :", position.coords.latitude);
+  // console.log("Longitude is :", position.coords.longitude);
+});
