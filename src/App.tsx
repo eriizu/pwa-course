3fb44562c22a90aeda1655c62aff0458f5594b64
Élise Philippe <elise@eiss.fr>
2021-01-29 17:41:16 +0100

    fix syntax warning

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 function urlBase64ToUint8Array(base64String: string) {
   var padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  var base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
+  var base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
 
   var rawData = window.atob(base64);
   var outputArray = new Uint8Array(rawData.length);
